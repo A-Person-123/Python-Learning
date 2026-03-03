002e32bf75fabaf45a4fa5c693cbd2e3b49ed199
A-Person-123 <demonkozik@gmail.com>
2026-03-03 19:32:42 +0000

    Madlibs game activity

---
+++ Learning 2.py
@@ -0,0 +1,14 @@
+## Second lesson, any task will be written and save here.
+## Some lines of code are added or modified by me
+
+## This is a madlibs game
+adjective1 = input("Enter an adjective: ")
+noun1 = input("Enter a noun: ")
+verb1 = input("Enter a verb ending with 'ing': ")
+verb2 = input("Enter another verb ending with 'ing': ")
+adjective2 = input("Enter another adjective: ")
+
+print(f"Today I ran to a {adjective1} park")
+print(f"The park was filled with {noun1}/s")
+print(f"The {noun1}/s were {verb1} and {verb2}")
+print(f"I felt very {adjective2} while the {noun1}/s were {verb1} and {verb2}")
