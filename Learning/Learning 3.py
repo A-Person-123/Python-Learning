ccd819cd20153c29543c5c93258368e6b8591b86
A-Person-123 <demonkozik@gmail.com>
2026-04-13 19:18:13 +0100

    Lesson 3 exercise

--- Learning/Learning 3.py
+++ Learning/Learning 3.py
@@ -1,2 +1,15 @@
 ## Third lesson, any task will be written and save here.
 ## Some lines of code are added or modified by me
+weight = float(input("What is your weight? "))
+unit = input("Kilograms or pounds? (Kg or Lbs) ")
+
+if unit == "Kg":
+    weight = weight * 2.205
+    unit = "Lbs."
+    print(f"Your weight is {round(weight, 3)} {unit}")
+elif unit == "Lbs":
+    weight = weight / 2.205
+    unit = "Kg."
+    print(f"Your weight is {round(weight, 3)} {unit}")
+else:
+    print(f"{unit} is not a valid unit")
